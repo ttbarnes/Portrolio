dfe375139410169462b64125fbd20736b9b91a8b
Tony Barnes <tony@tonybarnes.me>
2013-05-05 17:45:11 +0100

    JS tidy

---
+++ js/options-old.js
@@ -0,0 +1,38 @@
+ //$('div.item, div.item img').style.height = "";
+						 
+						  //options init
+							//if (defaultSettings.activeHoverStatesAll ==  true) { //activeHover all
+								 //activeHoverStates();
+								 //activeHoverToolTip();
+							//}
+							
+							//if (defaultSettings.activeHoverStates.activeHoverToolTipTitles == true) { //activeHover tooltip titles
+						  // activeHoverToolTip(); //toolTip titles only
+							//}
+						 
+							//if (defaultSettings.activeHoverStates.activeHoverToolTipFixedBtm == true) { //activeHover fixed btm
+							//	 activeHoverToolTipFixedBtm(); //toolTip fixed bottom
+							//}
+							
+							//else if (defaultSettings.activeHoverStatesAll ==  false) { //activeHover false
+								 //do nothing?
+							//}
+							/*
+							if (defaultSettings.activeHoverStates.activeHoverDim == true) { //activeHoverDim
+								 activeHoverStates();
+								 activeHoverStatesDim();
+							}
+							else if (defaultSettings.activeHoverStates.activeHoverDim == false) { //activeHoverDim false
+								//do nothing?
+							}
+							else {
+								activeHoverStatesDim();
+							}
+							
+							if (defaultSettings.activeHoverStates.activeHoverToolTipTitles == true) { //activeHoverTitles
+								 activeHoverToolTip(); //tooltip title stuff
+							}
+							else if (defaultSettings.activeHoverStates.activeHoverToolTipTitles == false) {
+								//do nothing
+							}*/
+							
--- js/portrolio.alpha.1.1.1.js
+++ js/portrolio.alpha.1.1.1.js
@@ -314,56 +314,15 @@
 							activeHoverStatesDim(); //items active/hover/dim states
 							activeHoverToolTip(); //item tooltip - active tooltip
 								 
-								 
 							/////////
 							//////
 							////
 							//
-							//refactor the options
+							//refactor the options - see external JS
 							//
 							////
 							//////
-							/////////	 
-								 
-						 //$('div.item, div.item img').style.height = "";
-						 
-						  //options init
-							//if (defaultSettings.activeHoverStatesAll ==  true) { //activeHover all
-								 //activeHoverStates();
-								 //activeHoverToolTip();
-							//}
-							
-							//if (defaultSettings.activeHoverStates.activeHoverToolTipTitles == true) { //activeHover tooltip titles
-						  // activeHoverToolTip(); //toolTip titles only
-							//}
-						 
-							//if (defaultSettings.activeHoverStates.activeHoverToolTipFixedBtm == true) { //activeHover fixed btm
-							//	 activeHoverToolTipFixedBtm(); //toolTip fixed bottom
-							//}
-							
-							//else if (defaultSettings.activeHoverStatesAll ==  false) { //activeHover false
-								 //do nothing?
-							//}
-							/*
-							if (defaultSettings.activeHoverStates.activeHoverDim == true) { //activeHoverDim
-								 activeHoverStates();
-								 activeHoverStatesDim();
-							}
-							else if (defaultSettings.activeHoverStates.activeHoverDim == false) { //activeHoverDim false
-								//do nothing?
-							}
-							else {
-								activeHoverStatesDim();
-							}
-							
-							if (defaultSettings.activeHoverStates.activeHoverToolTipTitles == true) { //activeHoverTitles
-								 activeHoverToolTip(); //tooltip title stuff
-							}
-							else if (defaultSettings.activeHoverStates.activeHoverToolTipTitles == false) {
-								//do nothing
-							}*/
-							
-							
+							/////////	 	
 					  }
 				 }).register("screen and (max-width:768px)", {
 					 match : function() { 
