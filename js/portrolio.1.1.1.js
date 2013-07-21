fbaea53a8e02ba6d873ec2b72457cc4c19f9a3a0
Tony Barnes <tony@tonybarnes.me>
2013-07-21 15:59:41 +0100

    Refactored everything - preparing for v1

--- js/portrolio.1.1.1.js
+++ js/portrolio.1.1.1.js
@@ -7,9 +7,9 @@
 	* http://tonybarnes.me
 	* No license or copyright - do what you like
 	*
-	* Last updated: 15/06/2013
+	* Last updated: 21/07/2013
 	* Reqruiments: 
-	* 1: jQuery
+	* 1: jQuery 2.0 (http://jquery.com/download/)
 	* 2: Media.match (https://github.com/weblinc/media-match)
 	* 3: Inview (http://github.com/protonet/jquery.inview)
 	*
@@ -19,38 +19,35 @@
 	var methods = {
 		init : function(options) {
 		
-		  //default settings
-			var defaultSettings = $.extend({ //default,public? settings
-				columnsNo: 3, //no. of colums (3, X, X)
-				columnsDiy: false, //cancel column width calculations
+			var defaultSettings = $.extend({ //default settings
+				columnsNo: 3, //no. of colums (2,3,6)
+				columnsDiy: false, //cancel column width calculations?
 				activeHoverStates: {
 					dim:true,
 					dimOnly:false,
 					toolTip:true, //tooltip, no slide
-					toolTipFixedBtm:false, //tooltip fixed at bottom (default will slide but can be switched off)
+					//toolTipFixedBtm:false, //tooltip fixed at bottom (default will slide but can be switched off)
 					toolTipSlide:true, //tooltip and slide
 				},
-				
 				pageSelectors: {
 					header: 'div.header',
 					footer: 'div.footer'
 				},
-				clrSchemes: true, //color schemes
-				  clrScheme: { 
-						darkBlue:true //darkBlue (lightBlue, darkBlue, black, white, grey)
-				  }
+				colorScheme: { //color schemes
+					black:true //darkBlue lightBlue, darkBlue, black, white, grey, none (transparent, white text)
+				} 
 			}, options);
 			
 			return this.each(function(){
 				
 				//private settings/vars
-				var $parentElm = $(this).children('div.streamInnerWrapper'); //parent element
+				var $parentElm = $(this).children('div.portrolioInnerWrapper'); //parent element
 				var $elm = $parentElm.children('div.item'); //individual items
 				var $elmImg = $elm.children('img')[0]; //item img
 				var elmImgMaxHeight = $('img').height(); //img height
 				
 				//column layout				
-				var $columnsAllowed = [2, 3, 6]; //allowed columns
+				var $columnsAllowed = [3, 6]; //allowed columns
 				var $columnsNoResult = defaultSettings.columnsNo; //get columns result
 				i = $columnsAllowed;
 				
@@ -58,106 +55,101 @@
 					var columsNoResult = defaultSettings.columnsNo; //get columns
 					if (defaultSettings.columsNoResult == $columnsAllowed[i]) { //need to figure out how to test this from the var instead of hard coded.
 					
-						if (defaultSettings.columnsNo == 2){ //2 columns
-							$parentElm.addClass('col2');
-						}
-						else if (defaultSettings.columnsNo == 3){ //3 columns
+						if (defaultSettings.columnsNo == 3){ //3 columns
 							$parentElm.addClass('col3');
 						} 
 						else if (defaultSettings.columnsNo == 6){ //6 columns
 							$parentElm.addClass('col6');
 						}
 						else {
-							console.log('error - please enter 2, 3 or 6 columns') //error
+							console.log('error - please enter 3 or 6 columns') //error
 							$parentElm.hide();
 						}
 					}
 					else {
-						console.log('error - please enter 2, 3 or 6 columns') //error
-							$parentElm.hide();
+						console.log('error - please enter 3 or 6 columns') //error
+						$parentElm.hide();
 					}
 				}
 				
-				function aHStatesDefault() { //active hover states
-					$elm.each(function() {
-						if (defaultSettings.activeHoverStates.dim == true || defaultSettings.activeHoverStates.dimOnly == true){
-							$parentElm.addClass('dimActive');
-							$(this).hover(function(e){
-								$elm.addClass('active');
-								var $elmHover = $(this);
-								$($elmHover).addClass('hovered');
-							},
-							function(e){
-								$elm.removeClass('active');		
-								$(this).removeClass('hovered');
-							});
+				function aHStatesDimming() { //active hover states dimming			
+					if (defaultSettings.activeHoverStates.dim == true || defaultSettings.activeHoverStates.dimOnly == true){
+						$parentElm.addClass('dimActive');
+					}
+				} 
+				
+				function aHStatesInit(){ //active hover states init - default active hover states (from original plugin)
+				  $($elm).bind({
+						mouseenter: function(e) {					
+							$elm.addClass('active');
+							var $elmHover = $(this);
+							$($elmHover).addClass('hovered');
+						},
+						mouseout: function(e) {
+							$elm.removeClass('active');		
+							$(this).removeClass('hovered');
 						}
-						
-						//if media match, do the hover..
-						if(defaultSettings.activeHoverStates.toolTip == true) {
-							$parentElm.addClass('dimActive');
-							$parentElm.addClass('ttipTitleActive');
-							if ($(this).find('span.title').length > 0) {
-								$(this).hover(function(e){
-									var $elmHover = $(this);
-									var $toolTip = $('<div class="toolTip"><span class="bg"/></div>').appendTo(this); //create toolTip and bg
-									$('div.toolTip').show();
+					});	
+				}
+				
+				function aHStatesToolTip() { //active hover states tooltip
+					$parentElm.addClass('ttipTitleActive');
+					ttipSetting001 = defaultSettings.activeHoverStates.toolTip == true;
+					ttipSetting002 = defaultSettings.activeHoverStates.toolTip == false;
+					ttipSetting003 = defaultSettings.activeHoverStates.toolTipSlide == true;
+					ttipSetting004 = defaultSettings.activeHoverStates.toolTipSlide == false;
+					
+					switch (true) {
+						case ttipSetting003:
+						$($elm).bind({
+							mouseenter: function testFunctionBinding (e) {
+								var $elmHover = $(this);
+								if ($($elmHover).find('span.title').length > 0) {
+									var $toolTip = $('<div class="toolTip toolTipSlideIn"><span class="bg"/></div>').appendTo(this); //create toolTip and bg
+									$('div.toolTip').show().animate({ bottom: "0" },050);
 									$($elmHover).children('span.title').appendTo('div.toolTip');
+								}				
+							},
+							mouseleave: function(e) {								 
+								$elm.removeClass('active');
+								$(this).removeClass('hovered');										
+								$('div.toolTip span.title').appendTo(this);
+								$('div.toolTip').remove();
+								$(this).removeClass('hovered');
+								$('div.toolTip').animate({ bottom: "-25px" }).hide();
+							},
+						});		
+						break;
+						
+						case ttipSetting001: case ttipSetting004:
+							$($elm).bind({
+								mouseenter: function testFunctionBinding (e) {
+									var $elmHover = $(this);
+									if ($($elmHover).find('span.title').length > 0) {
+										var $toolTip = $('<div class="toolTip"><span class="bg"/></div>').appendTo(this); //create toolTip and bg
+										$('div.toolTip').show();
+										$($elmHover).children('span.title').appendTo('div.toolTip');
+									}				
 								},
-								function(e){
+								mouseleave: function(e) {								 
 									$elm.removeClass('active');
 									$(this).removeClass('hovered');										
-									$('div.toolTip span.title').appendTo(this); //move title back to parent item
+									$('div.toolTip span.title').appendTo(this);
 									$('div.toolTip').remove();
 									$(this).removeClass('hovered');
 									$('div.toolTip').hide();
-								});		
-							}
-						}	
-								
-						if (defaultSettings.activeHoverStates.toolTipSlide == true) { //tooltip slide
-							$(this).hover(function(e){
-								var $elmHover = $(this);
-								$('div.toolTip').addClass('toolTipSlideIn').animate({ bottom: "0" },200);							
-							},
-							function(e){
-								$('div.toolTip').animate({ bottom: "-25px" }); //remove/reset tooltip
-							});
-						}
-						
-						if(defaultSettings.activeHoverStates.toolTipFixedBtm == true){ //toolTip fixed bottom
-							$($parentElm).addClass('toolTipFixedBtm');
-						}
-						
-						if (defaultSettings.activeHoverStates.dim == false || defaultSettings.activeHoverStates.dimOnly == true){
-							$parentElm.addClass('dimActive');
-							$(this).hover(function(e){
-								$elm.addClass('active');
-								var $elmHover = $(this);
-								$($elmHover).addClass('hovered');
-							},
-							function(e){
-								$elm.removeClass('active');		
-								$(this).removeClass('hovered');
-							});
-						}
-						
-						
-						if (defaultSettings.activeHoverStates.dim == false) { //dim false
-							if(($parentElm).hasClass('dimActive')){
-								$parentElm.removeClass('dimActive');
-							}
-						}
-						
-					});
-				} //active hover states END
-				
+								},
+							});		
+						break;										
+					}		
+				}
+			
 				var createOverlays = function createOverlays() { //create, append overlays
 				  var $overlayTop = $('<div class="overlay overlayTop"/>', { //create overlay top, set height
-						id: 'streamOverlayTop'
+						id: 'portrolioOverlayTop'
 					}).insertBefore($parentElm.children(':eq(0)'));
 					var $overlayBtm = $('<div class="overlay overlayBtm"/>', { //create overlay btm, set height
-						id: 'streamOverlayBtm'
+						id: 'portrolioOverlayBtm'
 					}).appendTo($parentElm);	
 				}
 				
@@ -181,13 +173,11 @@
 							$($overlayBtm).css({'display': 'block', 'position': 'fixed', 'bottom':'0'});
 						}
 					});
-					
 					$($elm3).bind('inview', function(event, visible) { //if 3rd item in view
 						if (visible) {
 							$($overlayBtm).css({'position': 'fixed', 'bottom':'0' });
 						}
 					});
-					
 					$($elmLast).bind('inview', function(event, visible) { //if last item in view
 						if (visible) {
 							$($overlayBtm).css({'position': 'absolute', 'bottom':'0' });
@@ -195,7 +185,7 @@
 							$($overlayBtm).css({'position': 'fixed', 'bottom':'0' });
 						}
 					});
-					$($footer).bind('inview', function(event, visible) { //if footer in view (this will be the immediate item after #streamWrapper)
+					$($footer).bind('inview', function(event, visible) { //if footer in view (this will be the immediate item after #portrolioWrapper)
 						if (visible) {
 							$($overlayTop).css({'position': 'absolute', 'top':'0' });
 						}
@@ -203,28 +193,29 @@
 							$($overlayTop).css({'position': 'fixed', 'top':'0' });
 						}
 					});	
-				}	
+				}
 				 
 				//color schemes
-				if (defaultSettings.clrSchemes == true) {
-				  if (defaultSettings.clrScheme.lightBlue == true){ //light blue
-						$parentElm.addClass('clrSchemeLightBlue');
-						}
-					else if (defaultSettings.clrScheme.darkBlue == true){ //dark blue
-						$parentElm.addClass('clrSchemeDarkBlue');
+				if (defaultSettings.colorScheme == 'lightBlue'){ //light blue
+					$parentElm.addClass('clrSchemeLightBlue');
 					}
-					else if (defaultSettings.clrScheme.black == true){ //black
-						$parentElm.addClass('clrSchemeBlack');
-					}
-					else if (defaultSettings.clrScheme.white == true){ //white
-						$parentElm.addClass('clrSchemeWhite');
-					}
-					else if (defaultSettings.clrScheme.grey == true){ //grey
-						$parentElm.addClass('clrSchemeGrey');
-					}
-					else if (defaultSettings.clrScheme.none == true){ //none
-						$parentElm.addClass('clrSchemeNone');
-					}
+				else if (defaultSettings.colorScheme == 'darkBlue'){ //dark blue
+					$parentElm.addClass('clrSchemeDarkBlue');
+				}
+				else if (defaultSettings.colorScheme == 'black'){ //black
+					$parentElm.addClass('clrSchemeBlack');
+				}
+				else if (defaultSettings.colorScheme == 'white'){ //white
+					$parentElm.addClass('clrSchemeWhite');
+				}
+				else if (defaultSettings.colorScheme == 'grey'){ //grey
+					$parentElm.addClass('clrSchemeGrey');
+				}
+				else if (defaultSettings.colorScheme == 'none'){ //none
+					$parentElm.addClass('clrSchemeNone');
+				}
+				else { //default
+					$parentElm.addClass('clrSchemeBlack');
 				}
 				
 				function itemHeightCalc() { //item/overlay height calc
@@ -258,47 +249,38 @@
 				}
 				
 				function itemHeightCalcReset(){ //item/overlay height calc reset
-					var $overlays = $('div.overlay'); //quickfix
-					var $elmImg = $('img'); //quickfix
+					var $overlays = $('div.overlay');
+					var $elmImg = $('img');
 					$overlays.remove();
-					if ($($elmImg).height == ('auto')) { //set auto height
-					}
-					else {
-						$elmImg.height('auto');
-					}
+					$elmImg.css({
+						'height':'auto',
+						'max-height':'none'
+					});
 				}
-				 
-				 
-				/////////
-				///////
-				////
-				//breakpoints
-				////
-				///////
-				/////////
-				function mediaMatchQ() { //matchMedia - make it all dynamic
-					
+				
+				function mediaMatchQ() { //breakpoints - matchMedia - make it all dynamic
 					var mql1 = window.matchMedia('screen and (min-width: 768px)');
 					var mql2 = window.matchMedia('screen and (max-width: 768px)');
 					var mql3 = window.matchMedia('screen and (min-width: 480px)');
 					var mql4 = window.matchMedia('screen and (max-width: 480px)');
 					
 					if (mql1.matches) {
-						console.log('mql1 matches - running')
 						itemHeightCalcReset(); //item/overlay height calc reset
-						aHStatesDefault();
+						
+						//new						
+						aHStatesInit(); //active hover states init
+						aHStatesDimming(); //active hover states dimming
+						var aHStatesToolTipActive = defaultSettings.activeHoverStates.toolTip; //if tooltip is not false, run
+						if(aHStatesToolTipActive != false) {
+							aHStatesToolTip();
+						}
 					}
-					
 					if (mql2.matches) {
-						console.log('mql2 matches - running')				
-						$elm.unbind('mouseenter mouseleave'); //unbind all hover events
 						$parentElm.removeClass('dimActive'); //remove class
-						$parentElm.removeClass('toolTipSlideIn'); //remove class
+						$parentElm.removeClass('ttipTitleActive'); //remove class
 					}
-					
 					if (mql4.matches) {
-						console.log('mql4 matches - running')									 
-						$('div.toolTip').remove();
+						$parentElm.removeClass('ttipTitleActive'); //remove class
 						$elm.hide().fadeIn(300);
 						createOverlays(); //create, append overlays
 						itemHeightCalc(); //item/overlay height calc
@@ -306,68 +288,55 @@
 					}
 			}
 			mediaMatchQ();
-
+		
+			var timerDelay = (function(){ //timer delay, see http://stackoverflow.com/questions/5489946/jquery-how-to-wait-for-the-end-or-resize-event-and-only-then-perform-an-ac
+				var timer = 0;
+				return function(callback, ms){
+					clearTimeout (timer);
+					timer = setTimeout(callback, ms);
+				};
+			})();
 				
-				
-				
-				
-				
-				
-				//temp removed to debug
-				var timerDelay = (function(){ //timer delay, see http://stackoverflow.com/questions/5489946/jquery-how-to-wait-for-the-end-or-resize-event-and-only-then-perform-an-ac
-					var timer = 0;
-					return function(callback, ms){
-						clearTimeout (timer);
-						timer = setTimeout(callback, ms);
-					};
-				})();
-				
-				$(window).resize(function() { //run after window resize is complete. Otherwise timer is reset.
-					timerDelay(function(){
-						function mediaMatchQTimed(){ //item height calculations
-							
-							var mql3 = window.matchMedia('screen and (min-width: 480px)');
-							var mql4 = window.matchMedia('screen and (max-width: 480px)');
-							
-								if (mql3.matches) {
-									console.log('mql3 matches - running')									 
-									itemHeightCalcReset(); //item/overlay height calc reset
-								}
-								
-								if (mql4.matches) {
-									console.log('mql4 matches - running')									 
-																			
-									$('div.toolTip span.title').hide();
-									$('div.toolTip').remove();
-									
-									$elm.hide().fadeIn(300);
-									createOverlays(); //create, append overlays
-									itemHeightCalc(); //item/overlay height calc
-									overlayInView(); //in view - overlays
-									
-								}
+			$(window).resize(function() { //run after window resize is complete. Otherwise timer is reset.
+				timerDelay(function(){
+					function mediaMatchQTimed(){ //item height calculations
+						var mql2 = window.matchMedia('screen and (min-width: 768px)');
+						var mql3 = window.matchMedia('screen and (min-width: 480px)');
+						var mql4 = window.matchMedia('screen and (max-width: 480px)');
+						if (mql2.matches) {
+							$($elm).bind('mouseenter');
+							aHStatesInit(); //active hover states init
+							aHStatesDimming(); //active hover states dimming								
+							var aHStatesToolTipActive = defaultSettings.activeHoverStates.toolTip; //if tooltip is not false, run
+							if(aHStatesToolTipActive != false) {
+								aHStatesToolTip();
+							}
 						}
-						mediaMatchQTimed();						
-					}, 500);
-				});
-				
-				
-				
-				//////////
-				////////
-				//////
-				//loading fails in Opera (mac)
-				/*
-	      var $loading = $('<div id="loading">loading...</div>', {}).insertBefore($parentElm); //create loading element
-				$loading.show(); //show the loading
-				$parentElm.hide(); //hide the stream
-				var $elmImg = $('div.item img'); //select images (quick-fix)
-				$elmImg.load(function(){ //after images loaded
-				  $loading.hide(); //hide the loading
-					$parentElm.fadeIn(800).show(); //fade in
-				});
-				*/
-				
+						if (mql3.matches) {
+							$('div.toolTip').remove();								 
+							itemHeightCalcReset(); //item/overlay height calc reset
+						}
+						if (mql4.matches) {
+							$($elm).unbind('mouseenter').hide().fadeIn(300); //unbind mouse over (this is key), then hide and fade
+							createOverlays(); //create, append overlays
+							itemHeightCalc(); //item/overlay height calc
+							overlayInView(); //in view - overlays
+						}
+					}
+					mediaMatchQTimed();						
+				}, 500);
+			});
+			
+			//loading fails in Opera (mac)
+			//
+			//var $loading = $('<div id="loading">loading...</div>', {}).insertBefore($parentElm); //create loading element
+			//$loading.show(); //show loading elms
+			//$parentElm.hide(); //hide parent
+			//var $elmImg = $('div.item img'); //select images (quick-fix)
+			//$elmImg.load(function(){ //after images loaded
+			//	$loading.hide(); //hide the loading
+			//	$parentElm.fadeIn(800).show(); //fade in
+			//});
 		  });
 		}
 	};
@@ -379,7 +348,7 @@
       return methods.init.apply( this, arguments );
     } 
 		else {
-      $.error('Error!');
+      $.error('Portrolio Error! Something has gone wrong. Please see the original codebase, or contact the author');
     } 
   };
 	return this;
